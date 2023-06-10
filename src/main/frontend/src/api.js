92399576999ec096657754956c209e13d46dd06c
yehor96 <yehor.shyriaiev@gmail.com>
2023-06-10 14:26:56 +0300

    test revert

--- src/main/frontend/src/api.js
+++ src/main/frontend/src/api.js
@@ -1,26 +1,26 @@
-// import axios from "axios";
+import axios from "axios";
 
 export const getMonthlyExpenses = async (date) => {
-  // try {
-  //   const response = await axios.get("/api/v1/expenses/monthly", {
-  //     params: {
-  //       month: date.month,
-  //       year: date.year,
-  //     },
-  //   });
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching data from server:", error);
-  //   throw error;
-  // }
+  try {
+    const response = await axios.get("/api/v1/expenses/monthly", {
+      params: {
+        month: date.month,
+        year: date.year,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data from server:", error);
+    throw error;
+  }
 };
 
 export const getCategories = async () => {
-  // try {
-  //   const response = await axios.get("/api/v1/categories");
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching data from server:", error);
-  //   throw error;
-  // }
+  try {
+    const response = await axios.get("/api/v1/categories");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data from server:", error);
+    throw error;
+  }
 };
